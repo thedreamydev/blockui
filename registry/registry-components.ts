333bb67f9565194722de5eb0d223568968127279
Shoaib Sayyed <sayyedshoaib57@gmail.com>
2025-01-18 16:01:49 +0530

    chore: added lib/utils as a file for wallet address component

--- registry/registry-components.ts
+++ registry/registry-components.ts
@@ -15,6 +15,10 @@ export const components: Registry = [
         path: "hooks/use-copy.ts",
         type: "registry:hook",
       },
+      {
+        path: "lib/utils.ts",
+        type: "registry:lib",
+      },
     ],
   },
 ];
